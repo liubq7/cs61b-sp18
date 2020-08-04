ecaaa44218fdd16a3b0b89b7810afbc4d76a525a
liubq7 <liubq97@gmail.com>
2020-08-04 20:00:53 +0800

    Printing the Universe

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -63,5 +63,14 @@ public class NBody {
             StdDraw.pause(10);
             time += dt;
         }
+
+        /* Printing the Universe */
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < planets.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                        planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+                        planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
     }
 }
