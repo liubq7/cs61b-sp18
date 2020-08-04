49e0769bbf64426184c554808aaddd9adc631166
liubq7 <liubq97@gmail.com>
2020-08-04 21:46:28 +0800

    test access

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
-    private static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;  // ？
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
