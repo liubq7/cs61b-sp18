d03164a483e7a76dd39443f9ba60ae50feebf519
liubq7 <liubq97@gmail.com>
2020-08-06 13:30:10 +0800

    format test

--- exercises/lec3lists1/IntList.java
+++ exercises/lec3lists1/IntList.java
@@ -36,28 +36,28 @@ public class IntList {
 
 	/** 
 	 *  Returns an IntList identical to L, but with
-     *  each element incremented by x. L is not allowed
-     *  to change. */
-    public static IntList incrList(IntList L, int x) {
-        if (L == null) {
+	*  each element incremented by x. L is not allowed
+	*  to change. */
+	public static IntList incrList(IntList L, int x) {
+		if (L == null) {
 			return L;
 		}
 		IntList Q = new IntList(L.first + x, incrList(L.rest, x));
 		return Q;
-    }
+	}
 
 	/** 
 	 *  Returns an IntList identical to L, but with
-     *  each element incremented by x. Not allowed to use
-     *  the 'new' keyword. */
-    public static IntList dincrList(IntList L, int x) {
-        if (L == null) {
+	 *  each element incremented by x. Not allowed to use
+	 *  the 'new' keyword. */
+	public static IntList dincrList(IntList L, int x) {
+		if (L == null) {
 			return L;
 		}
 		L.first += x;
 		L.rest = dincrList(L.rest, x);
 		return L;
-    }
+	}
 
 	public static void main(String[] args) {
 		IntList L = new IntList(15, null);
@@ -65,10 +65,10 @@ public class IntList {
 		L = new IntList(5, L);
 
 		System.out.println(L.size());
-        System.out.println(L.iterativeSize());
-        System.out.println(L.get(1));
+		System.out.println(L.iterativeSize());
+		System.out.println(L.get(1));
 
 		System.out.println(incrList(L, 3).get(1));
-        System.out.println(dincrList(L, 3).get(1));          
+		System.out.println(dincrList(L, 3).get(1));          
 	}
 } 
