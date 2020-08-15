97a05697b19ddb1381aa7569d1ca8d9c6fe6900c
liubq7 <liubq97@gmail.com>
2020-08-15 15:57:27 +0800

    Task 2: wordToDeque

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,2 +1,9 @@
 public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> deque = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 public class TestPalindrome {
     /*// You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
+     */
     static Palindrome palindrome = new Palindrome();
 
     @Test
@@ -14,5 +15,5 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
 }
