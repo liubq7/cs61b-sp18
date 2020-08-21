a86c7c56b8bfc878172bb957e1fca3d25e415157
liubq7 <liubq97@gmail.com>
2020-08-21 23:27:39 +0800

    Just For Fun: TTFAF

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,38 @@
+import synthesizer.GuitarString;
+
+public class GuitarHero {
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        GuitarString concert[] = new GuitarString[37];
+        for (int i = 0; i < concert.length; i++) {
+            concert[i] = new GuitarString(440.0 * Math.pow(2, (i - 24.0) / 12.0));
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index != 1) {
+                    concert[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (int i = 0; i < concert.length; i++) {
+                sample += concert[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < concert.length; i++) {
+                concert[i].tic();
+            }
+        }
+    }
+}
