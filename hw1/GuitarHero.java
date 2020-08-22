1ff7b0fbb3cdadb1a0103cd4fa2d9578e888bb37
liubq7 <liubq97@gmail.com>
2020-08-22 20:34:43 +0800

    check style

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -1,7 +1,7 @@
 import synthesizer.GuitarString;
 
 public class GuitarHero {
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
     public static void main(String[] args) {
         GuitarString concert[] = new GuitarString[37];
@@ -14,7 +14,7 @@ public class GuitarHero {
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                int index = keyboard.indexOf(key);
+                int index = KEYBOARD.indexOf(key);
                 if (index != 1) {
                     concert[index].pluck();
                 }
