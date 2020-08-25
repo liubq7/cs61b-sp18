4f8f1a2ed03816e41671d7f86aee8308c7bdd746
liubq7 <liubq97@gmail.com>
2020-08-25 20:37:28 +0800

    add class L

--- proj2/byog/Core/Hallway.java
+++ proj2/byog/Core/Hallway.java
@@ -7,43 +7,49 @@ import byog.TileEngine.Tileset;
 import java.util.Random;
 
 public class Hallway {
-    private Position start;
-    private Position end;
-    private Position corner;
+    private Position hStart;
+    private Position hEnd;
 
-    /* start在左边，corner与start同y */
-    private Hallway(Position p1, Position p2) {
-        if (p1.x <= p2.x) {
-            start = p1;
-            end = p2;
-        } else {
-            start = p2;
-            end = p1;
+    private static class L {
+        private Position start;
+        private Position end;
+        private Position corner;
+
+        /* start在左边，corner与start同y */
+        private L(Position p1, Position p2) {
+            if (p1.x <= p2.x) {
+                start = p1;
+                end = p2;
+            } else {
+                start = p2;
+                end = p1;
+            }
+
+            int pcx = end.x;
+            int pcy = start.y;
+            Position pc = new Position(pcx, pcy);
+            corner = pc;
         }
-
-        int pcx = end.x;
-        int pcy = start.y;
-        Position pc = new Position(pcx, pcy);
-        corner = pc;
     }
 
+
     /* 绘制L型 */
-    public static void drawL(TETile[][] world, Hallway h, TETile t) {
-        for (int x = h.start.x; x < h.corner.x; x += 1) {
-            world[x][h.start.y] = t;
+    public static void drawL(TETile[][] world, L l, TETile t) {
+        for (int x = l.start.x; x < l.corner.x; x += 1) {
+            world[x][l.start.y] = t;
         }
 
         int y1;
         int y2;
-        if (h.corner.y <= h.end.y) {
-            y1 = h.corner.y;
-            y2 = h.end.y;
+        if (l.corner.y <= l.end.y) {
+            y1 = l.corner.y;
+            y2 = l.end.y;
         } else {
-            y1 = h.end.y;
-            y2 = h.corner.y;
+            y1 = l.end.y;
+            y2 = l.corner.y;
         }
         for (int y = y1; y <= y2; y += 1) {
-            world[h.end.x][y] = t;
+            world[l.end.x][y] = t;
         }
     }
 
@@ -62,8 +68,8 @@ public class Hallway {
 
         Position p1 = new Position(25, 10);
         Position p2 = new Position(30, 20);
-        Hallway h = new Hallway(p1, p2);
-        drawL(world, h, Tileset.FLOWER);
+        L l = new Hallway.L(p1, p2);
+        drawL(world, l, Tileset.FLOWER);
 
 
         ter.renderFrame(world);
