e6678d289430808b91b584bbb4bc325b4b86b183
liubq7 <liubq97@gmail.com>
2020-08-25 21:13:40 +0800

    hallway ver2

--- proj2/byog/Core/Hallway.java
+++ proj2/byog/Core/Hallway.java
@@ -7,34 +7,38 @@ import byog.TileEngine.Tileset;
 import java.util.Random;
 
 public class Hallway {
+    /* start在左边，corner与start同y */
     private Position hStart;
     private Position hEnd;
+
+    private Hallway(Position p1, Position p2) {
+        if (p1.x <= p2.x) {
+            hStart = p1;
+            hEnd = p2;
+        } else {
+            hStart = p2;
+            hEnd = p1;
+        }
+    }
 
     private static class L {
         private Position start;
         private Position end;
         private Position corner;
 
-        /* start在左边，corner与start同y */
-        private L(Position p1, Position p2) {
-            if (p1.x <= p2.x) {
-                start = p1;
-                end = p2;
-            } else {
-                start = p2;
-                end = p1;
-            }
+        /* L不再判断起始位置，判断完了再创建类 */
+        private L(Position pStart, Position pEnd) {
+            start = pStart;
+            end = pEnd;
 
-            int pcx = end.x;
-            int pcy = start.y;
-            Position pc = new Position(pcx, pcy);
-            corner = pc;
+            Position pCorner = new Position(pEnd.x, pStart.y);
+            corner = pCorner;
         }
     }
 
 
     /* 绘制L型 */
-    public static void drawL(TETile[][] world, L l, TETile t) {
+    private static void drawL(TETile[][] world, L l, TETile t) {
         for (int x = l.start.x; x < l.corner.x; x += 1) {
             world[x][l.start.y] = t;
         }
@@ -53,6 +57,34 @@ public class Hallway {
         }
     }
 
+    /* 绘制end在上面的hallway（含围墙） */
+    private static void drawHallwayUp(TETile[][] world, Hallway h) {
+        L l1 = new L(h.hStart, h.hEnd);
+        L l2 = new L(h.hStart.upPosition(), h.hEnd.leftPosition());
+        L l3 = new L(h.hStart.downPosition(), h.hEnd.rightPosition());
+        drawL(world, l1, Tileset.FLOOR);
+        drawL(world, l2, Tileset.WALL);
+        drawL(world, l3, Tileset.WALL);
+    }
+    /* 绘制end在下面的hallway（含围墙） */
+    private static void drawHallwayDown(TETile[][] world, Hallway h) {
+        L l1 = new L(h.hStart, h.hEnd);
+        L l2 = new L(h.hStart.upPosition(), h.hEnd.rightPosition());
+        L l3 = new L(h.hStart.downPosition(), h.hEnd.leftPosition());
+        drawL(world, l1, Tileset.FLOOR);
+        drawL(world, l2, Tileset.WALL);
+        drawL(world, l3, Tileset.WALL);
+    }
+
+    /* 绘制hallway（含围墙） */
+    public static void drawHallway(TETile[][] world, Hallway h) {
+        if (h.hEnd.y > h.hStart.y) {
+            drawHallwayUp(world, h);
+        } else {
+            drawHallwayDown(world, h);
+        }
+    }
+
 
     public static void main(String[] args) {
         TERenderer ter = new TERenderer();
@@ -66,10 +98,14 @@ public class Hallway {
             }
         }
 
-        Position p1 = new Position(25, 10);
-        Position p2 = new Position(30, 20);
-        L l = new Hallway.L(p1, p2);
-        drawL(world, l, Tileset.FLOWER);
+        Position p1 = new Position(3, 10);
+        Position p2 = new Position(15, 13);
+        Position p3 = new Position(45, 10);
+        Position p4 = new Position(20, 20);
+        Hallway h1 = new Hallway(p1, p2);
+        Hallway h2 = new Hallway(p3, p4);
+        drawHallway(world, h1);
+        drawHallway(world, h2);
 
 
         ter.renderFrame(world);
--- proj2/byog/Core/Position.java
+++ proj2/byog/Core/Position.java
@@ -8,4 +8,17 @@ public class Position {
         x = X;
         y = Y;
     }
+
+    public Position leftPosition() {
+        return new Position(x - 1, y);
+    }
+    public Position rightPosition() {
+        return new Position(x + 1, y);
+    }
+    public Position upPosition() {
+        return new Position(x, y + 1);
+    }
+    public Position downPosition() {
+        return new Position(x, y - 1);
+    }
 }
