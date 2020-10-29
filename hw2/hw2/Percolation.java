ffa44d695a3ad4de29245db5dcf6d268f3034a86
liubq7 <liubq97@gmail.com>
2020-10-29 22:03:07 +0800

    fix a bug

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -18,7 +18,9 @@ public class Percolation {
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) throws IllegalArgumentException {
         if (N <= 0) {
-            throw new IllegalArgumentException("N should be greater than 0 but given N = " + N + ".");
+            throw new IllegalArgumentException(
+                    "N should be greater than 0 but given N = " + N + "."
+            );
         }
         this.N = N;
         openGridIn1D = new boolean[N * N];
@@ -40,7 +42,9 @@ public class Percolation {
     // open the site (row, col) if it is not open already
     public void open(int row, int col) throws IndexOutOfBoundsException {
         if (isInvalidIndex(row, col)) {
-            throw new IndexOutOfBoundsException("Invalid arguments given: row = " + row + " col = " + col + ".");
+            throw new IndexOutOfBoundsException(
+                    "Invalid arguments given: row = " + row + " col = " + col + "."
+            );
         }
         int indexIn1D = xyTo1D(row, col);
         openGridIn1D[indexIn1D] = true;
@@ -52,11 +56,11 @@ public class Percolation {
         updateConnection(row, col, row, col + 1);
 
         if (row == 0) {
-            topBottom.connected(indexIn1D, virtualTop);
-            top.connected(indexIn1D, virtualTop);
+            topBottom.union(indexIn1D, virtualTop);
+            top.union(indexIn1D, virtualTop);
         }
         if (row == N - 1) {
-            topBottom.connected(indexIn1D, virtualBottom);
+            topBottom.union(indexIn1D, virtualBottom);
         }
     }
 
@@ -65,13 +69,16 @@ public class Percolation {
         if (!isInvalidIndex(neighborRow, neighborCol) && isOpen(neighborRow, neighborCol)) {
             int neighborIndex = xyTo1D(neighborRow, neighborCol);
             topBottom.union(index, neighborIndex);
+            top.union(index, neighborIndex);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) throws IndexOutOfBoundsException {
         if (isInvalidIndex(row, col)) {
-            throw new IndexOutOfBoundsException("Invalid arguments given: row = " + row + " col = " + col + ".");
+            throw new IndexOutOfBoundsException(
+                    "Invalid arguments given: row = " + row + " col = " + col + "."
+            );
         }
         int indexIn1D = xyTo1D(row, col);
         return openGridIn1D[indexIn1D];
@@ -80,7 +87,9 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) throws IndexOutOfBoundsException {
         if (isInvalidIndex(row, col)) {
-            throw new IndexOutOfBoundsException("Invalid arguments given: row = " + row + " col = " + col + ".");
+            throw new IndexOutOfBoundsException(
+                    "Invalid arguments given: row = " + row + " col = " + col + "."
+            );
         }
         int indexIn1D = xyTo1D(row, col);
         return top.connected(indexIn1D, virtualTop);
