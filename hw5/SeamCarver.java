621319adc6685130c92bec992a2fbbbb74f0d12b
liubq7 <liubq97@gmail.com>
2021-01-02 16:26:46 +0800

    complete findVerticalSeam()

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -1,12 +1,17 @@
 import edu.princeton.cs.algs4.Picture;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SeamCarver {
     private Picture picture;
+    private int width;
+    private int height;
 
     public SeamCarver(Picture picture) {
         this.picture = picture;
+        width = picture.width();
+        height = picture.height();
     }
 
     // current picture
@@ -16,12 +21,12 @@ public class SeamCarver {
 
     // width of current picture
     public int width() {
-        return picture.width();
+        return width;
     }
 
     // height of current picture
     public int height() {
-        return picture.height();
+        return height;
     }
 
     // energy of pixel at column x and row y
@@ -37,14 +42,14 @@ public class SeamCarver {
         return gradient(left, right) + gradient(up, down);
     }
 
-    private int[] getNeighbor(int n, boolean isVertical) {
+    private int[] getNeighbor(int n, boolean isY) {
         int[] neighbors = new int[2];
-        if (isVertical) {
-            neighbors[0] = n > 0 ? n - 1 : height() - 1;
-            neighbors[1] = n + 1 < height() ? n + 1 : 0;
+        if (isY) {
+            neighbors[0] = n > 0 ? n - 1 : height - 1;
+            neighbors[1] = n + 1 < height ? n + 1 : 0;
         } else {
-            neighbors[0] = n > 0 ? n - 1 : width() - 1;
-            neighbors[1] = n + 1 < width() ? n + 1 : 0;
+            neighbors[0] = n > 0 ? n - 1 : width - 1;
+            neighbors[1] = n + 1 < width ? n + 1 : 0;
         }
         return neighbors;
     }
@@ -57,12 +62,88 @@ public class SeamCarver {
     }
 
 
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        double cost[][] = new double[width][height];
+        int path[][] = new int[width][height];
+
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                if (j == 0) {
+                    cost[i][j] = energy(i, j);
+                    path[i][j] = -1;
+                } else {
+                    double minCost = cost[i][j - 1];
+                    int parent = i;
+                    for (int n : getParent(i)) {
+                        if (cost[n][j - 1] < minCost) {
+                            minCost = cost[n][j - 1];
+                            parent = n;
+                        }
+                    }
+                    path[i][j] = parent;
+                    cost[i][j] = energy(i, j) + minCost;
+                }
+            }
+        }
+
+        double minTotalCost = cost[0][height - 1];
+        int minIndex = 0;
+        for (int i = 0; i < width; i++) {
+            if (cost[i][height - 1] < minTotalCost) {
+                minTotalCost = cost[i][height - 1];
+                minIndex = i;
+            }
+        }
+
+        int[] paths = new int[height];
+        int row = height - 1;
+        while (row >= 0) {
+            paths[row] = minIndex;
+            minIndex = path[minIndex][row];
+            row -= 1;
+        }
+
+        return paths;
+    }
+
+    private ArrayList<Integer> getParent(int n) {
+        ArrayList<Integer> parents = new ArrayList<>();
+        if (n > 0) {
+            parents.add(n - 1);
+        }
+        if (n + 1 < width) {
+            parents.add(n + 1);
+        }
+        return parents;
+    }
 
 
-//    public int[] findHorizontalSeam()            // sequence of indices for horizontal seam
-//
-//    // sequence of indices for vertical seam
-//    public int[] findVerticalSeam()
-//    public void removeHorizontalSeam(int[] seam)   // remove horizontal seam from picture
-//    public void removeVerticalSeam(int[] seam)     // remove vertical seam from picture
+
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from picture
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    // remove vertical seam from picture
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+
+    public static void main(String[] args) {
+        Picture p = new Picture("images/6x5.png");
+        SeamCarver sc = new SeamCarver(p);
+
+        int[] seam = sc.findVerticalSeam();
+        for (int i = 0; i < seam.length; i++) {
+            System.out.println(seam[i]);
+        }
+
+    }
 }
